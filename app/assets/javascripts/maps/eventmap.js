39c420ebce43247d8e5787721379ab5ed64df696
Abhi Ramesh <abhiramesh@gmail.com>
2013-11-23 22:15:18 -0500

    front home changes

--- app/assets/javascripts/maps/eventmap.js
+++ app/assets/javascripts/maps/eventmap.js
@@ -2,11 +2,13 @@ var initialLocation;
 var siberia = new google.maps.LatLng(60, 105);
 var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
 var browserSupportFlag =  new Boolean();
+var citymap = {};
+var cityCircle;
 
 function initialize() {
   var myOptions = {
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 
@@ -16,6 +18,33 @@ function initialize() {
     navigator.geolocation.getCurrentPosition(function(position) {
       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
       map.setCenter(initialLocation);
+
+   //    var marker = new google.maps.Marker({
+	  //   position: initialLocation,
+	  //   map: map,
+	  //   // animation: google.maps.Animation.BOUNCE,
+	  //   title:"Hello World!"
+	  // });
+
+	  citymap['philly'] = {
+		center: initialLocation,
+		number: 12
+	  };
+	  for (var city in citymap) {
+	    var numberOptions = {
+	      strokeColor: '#e96936',
+	      strokeOpacity: 0.7,
+	      strokeWeight: 1,
+	      fillColor: '#e96936',
+	      fillOpacity: 0.7,
+	      map: map,
+	      center: citymap[city].center,
+	      radius: 100
+	    };
+	    // Add the circle for this city to the map.
+	    cityCircle = new google.maps.Circle(numberOptions);
+	  }
+
     }, function() {
       handleNoGeolocation(browserSupportFlag);
     });
